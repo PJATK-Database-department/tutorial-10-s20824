341394127d56c70db2d8b96240fa027a7db5aad0
s20824 <antonimalinowski.kontakt@gmail.com>
2022-06-23 21:14:37 +0200

    add a model to an ASP.NET Core MVC app

---
+++ Task10/Task10/Data/Task10Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace Task10.Data
+{
+    public class Task10Context : DbContext
+    {
+        public Task10Context (DbContextOptions<Task10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; }
+    }
+}
